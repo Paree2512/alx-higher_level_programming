cd8cef3b6dbfaef13fde435a83d91470fae42f21
Paree2512 <benjaminsemevor@ymail.com>
2023-10-03 16:40:12 -0700

    100-print_tebahpla quick fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 
-val = 0
-b = 90
-
-for num in range(1, 27):
-    if num % 2 == 0:
-        val = b
-    else:
-        val = 123 - num
-
-        b -= 1
-
-        print(chr(val), end="")
+for c in range(122, 96, -1):
+    if c % 2 != 0:
+        c = (c - 32)
+        print("{}".format(chr(c)), end="")
