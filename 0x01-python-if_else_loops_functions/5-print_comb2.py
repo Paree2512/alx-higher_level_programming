c4e207667f911893d3a13958efcf1a76022816f6
Paree2512 <benjaminsemevor@ymail.com>
2023-10-04 00:44:57 -0700

    5-print_comb2 fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for n in range(100):
-    if n != 99:
-        print(f"{n:02d},", end=" ")
+for n in range(0, 100):
+    if n == 99:
+        print("{:02}".format(n))
     else:
-        print(f"{n:02d}")
+        print("{:02},".format(n), end=" ")
