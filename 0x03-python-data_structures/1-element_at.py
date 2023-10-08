636031c980177737b34d2bce6f2809ffd0657658
Paree2512 <benjaminsemevor@ymail.com>
2023-10-07 21:18:13 -0700

    1-element_at

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    num_elements = len(my_list)
+
+    if idx < 0:
+        return None
+    if idx >= num_elements:
+        return None
+    else:
+        return my_list[idx]
